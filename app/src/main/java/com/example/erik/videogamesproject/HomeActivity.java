52dd181a51fe1feb65029d2f8652663f6d9a7d4c
Erik Maraldi <erik.maraldi@gmail.com>
2016-10-10 22:32:23 +0200

    Login fatto

--- app/src/main/java/com/example/erik/videogamesproject/HomeActivity.java
+++ app/src/main/java/com/example/erik/videogamesproject/HomeActivity.java
@@ -1,16 +1,31 @@
 package com.example.erik.videogamesproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.firebase.client.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Firebase.setAndroidContext(this);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser() == null){
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        Toast.makeText(this, "Benvenuto " + user.getEmail(), Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/erik/videogamesproject/LoginActivity.java
+++ app/src/main/java/com/example/erik/videogamesproject/LoginActivity.java
@@ -1,26 +1,84 @@
 package com.example.erik.videogamesproject;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.client.Firebase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * Created by Erik on 10/10/2016.
  */
 
 public class LoginActivity extends AppCompatActivity{
-    TextView register;
+    private EditText txtLoginEmail;
+    private EditText txtLoginPassword;
+    private Button btnLogin;
+
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Firebase.setAndroidContext(this);
+
+        txtLoginEmail = (EditText) findViewById(R.id.txtLoginEmail);
+        txtLoginPassword = (EditText) findViewById(R.id.txtLoginPassword);
+
+        progressDialog = new ProgressDialog(this);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser() != null) {
+            startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+        }
+    }
+
+    public void ClickLogin(View view) {
+        String email = txtLoginEmail.getText().toString();
+        String password = txtLoginPassword.getText().toString();
+
+        progressDialog.setMessage("Attendere...");
+        progressDialog.show();
+
+        if(TextUtils.isEmpty(email)){
+            txtLoginEmail.setError("Email errata");
+            progressDialog.dismiss();
+        } else if (TextUtils.isEmpty(password)) {
+            txtLoginPassword.setError("Password errata");
+            progressDialog.dismiss();
+        } else {
+            firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        progressDialog.dismiss();
+                        startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                    }
+                }
+            });
+        }
+
+
+
     }
 
     public void OpenRegister(View view) {
