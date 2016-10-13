5cf96ed58666942d65ebd72e1dda62abdaa8e15a
Erik Maraldi <erik.maraldi@gmail.com>
2016-10-13 16:38:42 +0200

    Aggiunta classe videogame e HomeActivity aggioranta

--- app/src/main/java/com/example/erik/videogamesproject/HomeActivity.java
+++ app/src/main/java/com/example/erik/videogamesproject/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.example.erik.videogamesproject;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
@@ -8,21 +9,29 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class HomeActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     private NavigationView navigationView;
     private DrawerLayout drawerLayout;
+
+    private Firebase rootRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +41,13 @@ public class HomeActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(Color.WHITE);
 
         firebaseAuth = FirebaseAuth.getInstance();
 
-        if(firebaseAuth.getCurrentUser() == null){
+        rootRef = new Firebase("https://videogamesproject-cfd9f.firebaseio.com/");
+
+        if (firebaseAuth.getCurrentUser() == null) {
             startActivity(new Intent(this, LoginActivity.class));
         }
 
@@ -55,14 +67,14 @@ public class HomeActivity extends AppCompatActivity {
 
 
                 //Controlla se un elemento è cliccato o meno, se non lo è lo imposta a true
-                if(menuItem.isChecked()) menuItem.setChecked(true);
+                if (menuItem.isChecked()) menuItem.setChecked(true);
                 else menuItem.setChecked(false);
 
                 //Chiude il Drawer una volta cliccato un elemento
                 drawerLayout.closeDrawers();
 
                 //Controlla quale elemento viene cliccato ed esegue un azione appropriata per ogni elemento del Drawer
-                switch (menuItem.getItemId()){
+                switch (menuItem.getItemId()) {
 
                     //Lancia il fragment relativo ai videogiochi
                     case R.id.home:
@@ -71,25 +83,25 @@ public class HomeActivity extends AppCompatActivity {
                         setFragment(videogamesFragment);
                         return true;
                     case R.id.starred:
-                        Toast.makeText(getApplicationContext(),"Stared Selected",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "Stared Selected", Toast.LENGTH_SHORT).show();
                         return true;
                     case R.id.sent_mail:
-                        Toast.makeText(getApplicationContext(),"Send Selected",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "Send Selected", Toast.LENGTH_SHORT).show();
                         return true;
                     case R.id.drafts:
-                        Toast.makeText(getApplicationContext(),"Drafts Selected",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "Drafts Selected", Toast.LENGTH_SHORT).show();
                         return true;
                     case R.id.allmail:
-                        Toast.makeText(getApplicationContext(),"All Mail Selected",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "All Mail Selected", Toast.LENGTH_SHORT).show();
                         return true;
                     case R.id.trash:
-                        Toast.makeText(getApplicationContext(),"Trash Selected",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "Trash Selected", Toast.LENGTH_SHORT).show();
                         return true;
                     case R.id.spam:
-                        Toast.makeText(getApplicationContext(),"Spam Selected",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "Spam Selected", Toast.LENGTH_SHORT).show();
                         return true;
                     default:
-                        Toast.makeText(getApplicationContext(),"Errore",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "Errore", Toast.LENGTH_SHORT).show();
                         return true;
 
                 }
@@ -98,7 +110,7 @@ public class HomeActivity extends AppCompatActivity {
 
         //Inizializzaione Drawer Layout e ActionBarToggle
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
-        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout, toolbar, R.string.openDrawer,R.string.closeDrawer){
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer) {
 
             @Override
             public void onDrawerClosed(View drawerView) {
@@ -118,9 +130,45 @@ public class HomeActivity extends AppCompatActivity {
 
         //Chiamata al metodo syncState necessaria per visualizzare l'icona hamburger del Drawer
         actionBarDrawerToggle.syncState();
+
+
+        rootRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                Iterable<DataSnapshot> list = dataSnapshot.child("Videogames").getChildren();
+
+                while (list.iterator().hasNext()) {
+
+                    DataSnapshot data = list.iterator().next();
+
+                    ArrayList listPlatform = (ArrayList) data.child("Platform").getValue();
+                    Date releaseDate = new Date(Integer.parseInt(data.child("Release date").child("Anno").getValue().toString()),
+                            Integer.parseInt(data.child("Release date").child("Mese").getValue().toString()),
+                            Integer.parseInt(data.child("Release date").child("Giorno").getValue().toString()));
+
+
+                    Videogame videogame = new Videogame(data.child("Title").getValue().toString(), data.child("Genres").getValue().toString(),
+                            data.child("Developer").getValue().toString(), data.child("Publishers").getValue().toString(), listPlatform,
+                            releaseDate, Double.parseDouble(data.child("Rating").getValue().toString()), data.child("Plot").getValue().toString(),
+                            data.child("Trailer").getValue().toString(), data.child("Image").getValue().toString());
+
+                    Log.e("Ciaooooooooo", videogame.getReleaseDate().toString());
+
+                }
+                //dataSnapshot.child("Videogames").child("Dragon Age Origins ");
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
     }
 
-    public void setFragment(Fragment fragment){
+    //Metodo che server per settare un fragment, utilizzato nel navigation drawer quando si clicca su un elemento del menu
+    public void setFragment(Fragment fragment) {
         android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.frame, fragment);
         fragmentTransaction.commit();
---
+++ app/src/main/java/com/example/erik/videogamesproject/Videogame.java
@@ -0,0 +1,118 @@
+package com.example.erik.videogamesproject;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created by Marco on 13/10/2016.
+ */
+
+public class Videogame {
+
+    private String title;
+    private String genres;
+    private String developer;
+    private String publishers;
+    private ArrayList<String> platforms;
+    private Date releaseDate;
+    private double rating;
+    private String plot;
+    //private String description;
+    private String trailer;
+    private String image;
+
+
+    public Videogame(String title, String genres, String developer, String publishers, ArrayList<String> platforms,
+                     Date releaseDate, double rating, String plot, String trailer,String image) {
+
+        this.title = title;
+        this.genres = genres;
+        this.developer = developer;
+        this.publishers = publishers;
+        this.platforms = platforms;
+        this.releaseDate = releaseDate;
+        this.rating = rating;
+        this.plot = plot;
+        this.trailer = trailer;
+        this.image = image;
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenres() {
+        return genres;
+    }
+
+    public void setGenres(String genres) {
+        this.genres = genres;
+    }
+
+    public String getDeveloper() {
+        return developer;
+    }
+
+    public void setDeveloper(String developer) {
+        this.developer = developer;
+    }
+
+    public String getPublishers() {
+        return publishers;
+    }
+
+    public void setPublishers(String publishers) {
+        this.publishers = publishers;
+    }
+
+    public ArrayList<String> getPlatforms() {
+        return platforms;
+    }
+
+    public void setPlatforms(ArrayList<String> platforms) {
+        this.platforms = platforms;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getPlot() {
+        return plot;
+    }
+
+    public void setPlot(String plot) {
+        this.plot = plot;
+    }
+
+    public String getTrailer() {
+        return trailer;
+    }
+
+    public void setTrailer(String trailer) {
+        this.trailer = trailer;
+    }
+
+    public String getImage(){ return image; }
+
+    public void setImage(String image) { this.image = image; }
+
+
+}
