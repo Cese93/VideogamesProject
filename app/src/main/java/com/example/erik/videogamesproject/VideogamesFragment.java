9215a396283c796358acf3ae0626c8976293adb2
Erik <erik.maraldi@gmail.com>
2016-10-17 14:00:51 +0200

    Aggiornamento VideogameFragment

--- app/src/main/java/com/example/erik/videogamesproject/VideogamesFragment.java
+++ app/src/main/java/com/example/erik/videogamesproject/VideogamesFragment.java
@@ -1,69 +1,67 @@
 package com.example.erik.videogamesproject;
 
+import android.graphics.Color;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.ListViewCompat;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.firebase.client.Firebase;
-import com.firebase.ui.database.FirebaseListAdapter;
-import com.google.firebase.FirebaseApp;
+import com.firebase.client.collection.LLRBNode;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.yqritc.recyclerviewflexibledivider.HorizontalDividerItemDecoration;
 
-import java.util.ArrayList;
-import java.util.Date;
+import org.w3c.dom.Text;
+
 
 /**
  * Created by Erik on 12/10/2016.
  */
 
 public class VideogamesFragment extends Fragment {
-    private RecyclerView recyclerView;
-    private Firebase rootRef;
-    private FirebaseListAdapter<Videogame> prova;
-    DatabaseReference df;
+    private RecyclerView recyclerViewVideogames;
+    private FirebaseRecyclerAdapter videogamesAdapter;
+    private DatabaseReference databaseReference;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.videogames_fragment, container, false);
 
+        RecyclerView recyclerViewVideogames = (RecyclerView) v.findViewById(R.id.recyclerViewVideogames);
+        recyclerViewVideogames.addItemDecoration(new HorizontalDividerItemDecoration.Builder(getContext()).build());
+        recyclerViewVideogames.setHasFixedSize(true);
+        recyclerViewVideogames.setLayoutManager(new LinearLayoutManager(getContext()));
 
-ListView listView = (ListView) v.findViewById(R.id.listViewVideogames);
-        df = FirebaseDatabase.getInstance().getReferenceFromUrl("https://videogamesproject-cfd9f.firebaseio.com/").child("Videogames");
-        prova = new FirebaseListAdapter<Videogame>(
-                getActivity(),
+        databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://videogamesproject-cfd9f.firebaseio.com/Videogames");
+        videogamesAdapter = new FirebaseRecyclerAdapter<Videogame, ViewHolderVideogames>(
                 Videogame.class,
-                android.R.layout.simple_list_item_1,
-                df
+                R.layout.videogames_row_layout,
+                ViewHolderVideogames.class,
+                databaseReference
+
         ) {
             @Override
-            protected void populateView(View v, Videogame model, final int position) {
+            protected void populateViewHolder(ViewHolderVideogames viewHolder, Videogame model, final int position) {
+                viewHolder.txtTitle.setText(model.getTitle().toString());
+                viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
 
-                TextView textView = (TextView) v.findViewById(android.R.id.text1);
-                textView.setText(model.getTitle().toString());
-                Log.e("PROVAAAAAAAAAAAA", model.getImage().toString());
-                Log.e("PROVAAAAAAAAAAAA", String.valueOf(model.getReleaseDate().getMonth()));
-
-                v.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        Toast.makeText(getContext(), "Elemento in posizione: " + position, Toast.LENGTH_LONG).show();
+                        Toast.makeText(getContext(), "Posizione: " + position, Toast.LENGTH_LONG).show();
                     }
                 });
             }
+
         };
 
-        listView.setAdapter(prova);
+        recyclerViewVideogames.setAdapter(videogamesAdapter);
 
         return v;
     }
@@ -74,12 +72,13 @@ ListView listView = (ListView) v.findViewById(R.id.listViewVideogames);
 
     }
 
-    public static class VideogamesViewHolder extends RecyclerView.ViewHolder {
-        TextView textView;
+    public static class ViewHolderVideogames extends RecyclerView.ViewHolder {
+        TextView txtTitle;
 
-        public VideogamesViewHolder(View itemView) {
+        public ViewHolderVideogames(View itemView) {
             super(itemView);
-            textView = (TextView) itemView.findViewById(android.R.id.text1);
+            itemView.setSelected(true);
+            txtTitle = (TextView) itemView.findViewById(R.id.txtTitle);
 
         }
     }
