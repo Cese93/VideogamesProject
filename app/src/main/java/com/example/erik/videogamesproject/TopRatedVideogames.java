b6b3c29a480070f51bb01297c14835bc298c9f28
Erik <erik.maraldi@gmail.com>
2016-10-21 11:06:03 +0200

    Inizio tabs1

--- app/src/main/java/com/example/erik/videogamesproject/TopRatedVideogames.java
+++ app/src/main/java/com/example/erik/videogamesproject/TopRatedVideogames.java
@@ -15,5 +15,6 @@ public class TopRatedVideogames extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.tabs_layout, container, false);
+
     }
 }
