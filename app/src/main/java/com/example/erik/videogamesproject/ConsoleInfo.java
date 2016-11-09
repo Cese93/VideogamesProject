5392812b21de7d6570c11b0a140f317091818796
Erik <erik_93@live.it>
2016-11-09 10:37:16 +0100

    Prova

---
+++ app/src/main/java/com/example/erik/videogamesproject/ConsoleInfo.java
@@ -0,0 +1,10 @@
+package com.example.erik.videogamesproject;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by erik_ on 09/11/2016.
+ */
+
+public class ConsoleInfo extends AppCompatActivity {
+}
